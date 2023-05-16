26ff03deb514370db4288f80da049dfef6bdda80
Kamvelihle Sikalana <86585438+SmasherK@users.noreply.github.com>
2023-05-16 13:49:44 +0200

    Update code.js

--- script/code.js
+++ script/code.js
@@ -4,7 +4,7 @@ console.log(lastElement);
 subjects.push(lastElement)
 let mid= Math.floor(subjects.length/2);
 if ((subjects.length%2)=0){
-  console.log(subjects[mid] +' '+subjects[mid+1]
+  console.log(subjects[mid-1] +' '+subjects[mid]
 }else{
 console.log(subjects[mid]);
 }
