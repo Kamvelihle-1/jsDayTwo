885bb98bf98f72dc18f3fee591da8a780cdb8230
Kamvelihle Sikalana <86585438+SmasherK@users.noreply.github.com>
2023-05-16 13:47:34 +0200

    Update code.js

--- script/code.js
+++ script/code.js
@@ -3,4 +3,8 @@ let lastElement = subjects.pop()
 console.log(lastElement);
 subjects.push(lastElement)
 let mid= Math.floor(subjects.length/2);
+if ((subjects.length%2)=0){
+  console.log(subjects[mid] +' '+subjects[mid+1]
+}else{
 console.log(subjects[mid]);
+}
